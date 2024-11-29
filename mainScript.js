61c7a2a2062d7aed016657a0c7bce1bbe7a9c1f2
Richard <76806937+RixCrafts@users.noreply.github.com>
2024-11-29 09:48:38 -0500

    Start implementing carousel funtionality

--- mainScript.js
+++ mainScript.js
@@ -12,3 +12,32 @@ menuCloseBtn.addEventListener('click', () => {
     menuContainer.classList.toggle('active');
 });
 
+// Carousel functionality
+
+const carouselTrack = document.querySelector(".carousel-track");
+const carouselSlides = document.querySelectorAll(".carousel-slide");
+const btnPrev = document.querySelector(".prev");
+const btnNext = document.querySelector(".next");
+const itemWidth = document.querySelector(".carousel-slide").offsetWidth;
+
+btnPrev.addEventListener('click', () => handleClick("prev"));
+btnNext.addEventListener('click', () => handleClick("next"));
+
+// TODO: Implement JS for opacity effect
+// let slideSelected = 0
+// carouselSlides[slideSelected].classList.toggle("carousel-slide-active");
+console.log(itemWidth);
+
+function handleClick(direction){
+    if(direction === "prev"){
+        carouselTrack.scrollBy({
+            left: -itemWidth,
+            behavior: "smooth"
+        });
+    } else{
+        carouselTrack.scrollBy({
+            left: itemWidth,
+            behavior: "smooth"
+        });
+    }
+}
